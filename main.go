a95d399ac33d6b9f865a506470de0bdcc122e804
FrankieBoyC <fskiddles.covi@gmail.com>
2024-03-21 23:33:48 -0500

    Second Commit

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
-package quizGame
+package main
 
 import "fmt"
 
 func main() {
 	fmt.Println("Welcome to my quiz game!")
+
+	name := "Frank"
+	age := 21
+	fmt.Printf("Hello %v, you are %v!", name, age)
+	// You are at 27 minutes on the video
+	// https://www.youtube.com/watch?v=LHhsNa_Kgns&t=3529s
 }
